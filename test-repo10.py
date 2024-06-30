812f5b7d78715a4557109022b65482d71aa322de
Michael1182mitnick <56063336+Michael1182mitnick@users.noreply.github.com>
2024-06-29 23:03:24 -0800

    Create test-repo10.py

---
+++ test-repo10.py
@@ -0,0 +1,5 @@
+# Vertical integer from zero to five
+bacon = 0           # variable bacon
+while bacon <= 5:   # less than equal to five 
+    print(f'bacon is {bacon}')      # using f string 
+    bacon = bacon + 1
